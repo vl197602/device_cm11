a79903b7c443d4d8fd9e10c56752da6ce51bc348
vl197602 <radarga@gmail.com>
2013-12-09 23:57:09 +0300

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.k2_ul.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.tc2.rc
+LOCAL_SRC_FILES		:= etc/init.k2_ul.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
